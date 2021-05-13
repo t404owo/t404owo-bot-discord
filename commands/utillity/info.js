9205a5165b285e68a2ca6dee03c842ba92a408bc
t404owo <84060752+t404owo@users.noreply.github.com>
2021-05-13 09:31:45 +0200

    Update info.js

--- commands/utillity/info.js
+++ commands/utillity/info.js
@@ -1,14 +1,14 @@
 const discord = require('discord.js')
 exports.run = (bot, message, args) => {
-  const version = "1.0.0(Beta)";
+  const version = "1.0.0";
   let embed = new discord.MessageEmbed()
   .setAuthor(bot.user.tag, bot.user.avatarURL)
         .setTitle("Bot Information")
       .addField("Bot Name", bot.user.username, true)
       .addField("Version", version, true)
       .setThumbnail(bot.user.avatarURL({dynamic:true, size: 1024}))
-      .addField("Bot Owner/Lead Developer", "[🌻] Albedo#4704", true)
-      .addField("Bot Developers", "[404]-QuangBùi#6288, t404owo#2452", true)
+      .addField("Bot Owner/Lead Developer", "t404owo#2452", true)
+      //.addField("Bot Developers", "[404]-QuangBùi#6288, t404owo#2452", true)
       //.addField("Bot's Arcaea Wiki Members","t404owo#2452 mitsuko21#3187, TriPizza#7407",true)
       //.setFooter("")
       .setColor("#0affaf")
