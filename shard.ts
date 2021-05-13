145853e36a2448a538d8719e479a8e244f9123c1
t404owo <84060752+t404owo@users.noreply.github.com>
2021-05-13 09:29:42 +0200

    Update shard.ts

--- shard.ts
+++ shard.ts
@@ -2,9 +2,7 @@
 import fetch from "node-fetch"
 setInterval(async () => {
 
-  await fetch(`https://${process.env.PROJECT_DOMAIN}.glitch.me`)//main projects site
-  await fetch("https://404-bot-discord.glitch.me")
-  await fetch("https://hikari-bot-discord.glitch.me")
+  await fetch(`https://${process.env.PROJECT_DOMAIN}.glitch.me`)//main glitch projects site
 }, 60000)
 import {ShardingManager} from "discord.js"
 export const token = process.env.DISCORD_BOT_TOKEN;
