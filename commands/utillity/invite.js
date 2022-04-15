f630dce6e572076e9dd4bbc0d272766507fd8b2d
t404owo <84060752+t404owo@users.noreply.github.com>
2022-04-16 01:12:49 +0200

    Update invite.js

--- commands/utillity/invite.js
+++ commands/utillity/invite.js
@@ -18,7 +18,7 @@ exports.run = (bot, message, args) => {
     });   
 }
 exports.interaction= async(bot, interaction, args) =>{
-  let link = `https://discord.com/oauth2/authorize?client_id=${bot.user.id}&scope=bot+applications.commands&permissions=8989934591`
+  let link = `https://discord.com/api/oauth2/authorize?client_id=${bot.user.id}&scope=bot+applications.commands&permissions=1099511627775`
 let embed = new Discord.MessageEmbed()
       .setColor(process.env.DISCORD_BOT_EMBED_COLOR||'#0affaf')
        .setTitle("Invite me! <:koulove:801419554156445726>")
