72e9ffe8b3c21f3237773edcd66495957388f63f
t404owo <tags4owo@gmail.com>
2022-07-25 23:00:37 +0200

    Update shard.ts

--- src/shard.ts
+++ src/shard.ts
@@ -4,9 +4,7 @@ const server = express();
 server.all('/', (req: any, res: any) => {
   res.sendStatus(200)
 })
- 
-server.listen(3000, () => { console.log("Server is Ready!!" + Date.now()) });
-server.listen(8000, () => { console.log("Server is Ready!!" + Date.now()) });
+server.listen(process.env.PORT||8000, () => { console.log("Server is Ready!!" + Date.now()) });
 import { ShardingManager } from "discord.js";
 const manager = new ShardingManager("./src/main.jsx", {
   token: process.env.DISCORD_BOT_TOKEN,
